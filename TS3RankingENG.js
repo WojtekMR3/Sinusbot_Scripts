3e1a0fd8a6861e21966f96c2757e03deec931cfb
R3flex windows <r3flexmlg@gmail.com>
2020-04-05 00:43:34 +0200

    Update 1.1

--- TS3RankingENG.js
+++ TS3RankingENG.js
@@ -1,14 +1,34 @@
 registerPlugin({
     name: 'TS3 Ranking',
-    version: '1.0',
-    description: 'This script will add servergroups to the client upon the given hours.',
+    version: '1.1',
+    description: 'Count time. Grant levels. Display top clients on channel.',
     author: 'R3flex <r3flexmlg@gmail.com>',
     vars: {
-    afirstGroup: {
+    first_group_id: {
       title: 'Specify the server group ID of the first level.',
       type: 'number'
     },
-    hoursArray: {
+    enable_migration: {
+      title: 'Enable migrations',
+      type: 'checkbox'
+    },
+    migration_export_amount: {
+      title: 'Entries amount to export.',
+      type: 'number',
+      conditions: [{
+          field: 'enable_migration',
+          value: 1,
+      }]
+    },
+    migration_sgid: {
+      title: 'SGID needed to execute migration commands.',
+      type: 'number',
+      conditions: [{
+          field: 'enable_migration',
+          value: 1,
+      }]
+    },
+    hours_array: {
       title: 'Config',
       type: 'array',
       vars: [
@@ -20,7 +40,7 @@ registerPlugin({
           placeholder: '1'
         }]
     },
-    ignoredGroups: {
+    ignored_groups: {
       title: 'Ignored Group.',
       type: 'array',
       vars: [
@@ -33,31 +53,31 @@ registerPlugin({
         }
       ]
     },
-    AFKthreshold: {
+    AFK_threshold: {
       title: 'Specify the anty afk threshold in minutes (After this amount of minutes, the script will stop adding the time.)',
       type: 'number'
     },
-    drankingChannel: {
+    ranking_channel: {
       title: 'TOP 10 ranking channel ID.',
-      type: 'number',
+      type: 'channel',
       placeholder: 'Where to display ranking?'
     },
-    etopChannelRecords: {
+    top_channel_records: {
       title: 'How many records to display.',
       type: 'number',
       placeholder: '10'
     },
-    recordaNameColor: {
+    record_name_color: {
       title: 'Nicks Color',
       type: 'string',
       placeholder: '#FFF'
     },
-    recordbTimeColor: {
+    record_time_color: {
       title: 'Time Color',
       type: 'string',
       placeholder: '#FFF'
     },
-    recordcFormatColor: {
+    record_format_color: {
       title: 'Format Color',
       type: 'string',
       placeholder: '#FFF'
@@ -67,6 +87,12 @@ registerPlugin({
 }
 }, function main(sinusbot, config) {
 
+var event = require('event');
+var store = require('store');
+var engine = require('engine');
+var backend = require('backend');
+var format = require('format');
+
 var delayTime = 30;
 // GLOBALS
 var dataArray = [];
@@ -74,32 +100,68 @@ var firstDataArray = [];
 var sortable = [];
 var botList = [];
 var allLevelGroups = [];
-var allIgnoredGroups = [];
-var fg = config.afirstGroup;
+var allignored_groups = [];
+var fg = config.first_group_id;
 var arrayOfGroups = [];
-var limit = config.etopChannelRecords;
+var limit = config.top_channel_records || 10; 
+var channel = backend.getChannelByID(config.ranking_channel);
+const afk_time = config.AFK_threshold || 15;
+const list_nick_color = config.record_name_color || '#33BB00'; 
+const list_time_color = config.record_time_color || '#EEEE00';
+const list_format_color = config.record_format_color || '#BAE9FF';
+// Migration
+const migration_enabled = config.enable_migration;
+const migration_export_amount = config.migration_export_amount || 50;
+const migration_sgid = config.migration_sgid || -100;
 
 // Change all level groups from config from object to an array.
-for (var iter = 0; iter < config.hoursArray.length+1; iter++) {
+for (var iter = 0; iter < config.hours_array.length+1; iter++) {
   allLevelGroups.push(fg+iter);
 }
 
 // Change all ignored groups from config from object to an array.
-for (var iter = 0; iter < config.ignoredGroups.length; iter++) {
-  allIgnoredGroups.push(config.ignoredGroups[iter].ignoredGroup);
+for (var iter = 0; iter < config.ignored_groups.length; iter++) {
+  allignored_groups.push(config.ignored_groups[iter].ignoredGroup);
 }
 
-var event = require('event');
-var store = require('store');
-var engine = require('engine');
-var backend = require('backend');
-var helpers = require('helpers');
+event.on('chat', function(ev) {
+  if (ev.mode != 1) return;
+  let client = ev.client;
+  var text = ev.text;
+  if (text == "!export_db_old") {
+    // Check if migration flag is enabled.
+    if (!migration_enabled) return client.chat(format.color(format.bold("Migrations are not enabled!"), "#d2b72f"));
+    // Check if migration is enabled and client has permissions.
+    if (isNotInServerGroup(client.getServerGroups(), migration_sgid)) return client.chat(format.color(format.bold("You dont have permissions to use migration!"), "#d2b72f"));
+    let keys = store.getKeys();
+    let db = [];
+    keys.forEach(function(key) {
+      let entry = store.get(key);
+      db.push(entry[0]);
+    });
+    // Sort db
+    let db_sorted = db.sort((a, b) => new Number(b.time) - new Number(a.time));
+    // Apply export amount limit.
+    if (db_sorted.length > migration_export_amount) db_sorted.length = migration_export_amount;
+    if (db_sorted.length == 0) return client.chat(format.bold(`${format.color("Exporting failed!", "#ff3e3e")} Database is empty!`));
+    // Format db to match version 2.0+ {uid: {"nick", "time"}}
+    let export_db = {};
+    db_sorted.forEach(function(entry) {
+      let uid = entry.uid;
+      let nick = entry.nick;
+      let time = entry.time;
+      export_db[uid] = {nick: nick, time: time};
+    });
+    client.chat(format.bold(`${format.color("Exporting succesful!", "#00bf00")} Exported ${db_sorted.length} sets`));
+    client.chat(`${format.bold("!import_db")} ${JSON.stringify(export_db)}`);
+  }
+});
 
 event.on('clientMove', function(ev) {
   if(!ev.fromChannel) {
     var evKeys = store.getKeys();
     var evClientSGs = ev.client.getServerGroups();
-    if (isNotInServerGroup(evClientSGs, config.ignoredGroups)) { // the client is not bot
+    if (isNotInServerGroup(evClientSGs, config.ignored_groups)) { // the client is not bot
       if (isNotInServerGroup(evClientSGs, allLevelGroups)) { // check if client already has any of level groups
         // group adder
         var evFoundClient = false;
@@ -158,7 +220,7 @@ for(var iterator = 0; iterator < all_users.length; iterator++) {
         arrayOfGroups.push(parseInt(clientServerGroups[i].id()));
       } 
 
-  if (!isNotInServerGroup(clientServerGroups, allIgnoredGroups)) {
+  if (!isNotInServerGroup(clientServerGroups, allignored_groups)) {
     botList.push(clientUid);
     if (!isNotInServerGroup(clientServerGroups, allLevelGroups)) {
 
@@ -173,7 +235,7 @@ for(var iterator = 0; iterator < all_users.length; iterator++) {
   }
   // ANTI AFK
   var isAFK = false;
-  if ((user.getIdleTime()/1000) > (config.AFKthreshold*60)) {
+  if ((user.getIdleTime()/1000) > (afk_time*60)) {
     isAFK = true;
     continue;
   } 
@@ -201,7 +263,7 @@ if (store.get(allKeys[i])) {
 var object = store.get(allKeys[i])[0];
 
 if (iterationCount === 0) {
-	sortable.push([object.nick, object.time, object.uid]);
+  sortable.push([object.nick, object.time, object.uid]);
 }
 
       // CLIENT FOUND!
@@ -209,7 +271,6 @@ if (iterationCount === 0) {
         var found = true;
         var foundKey = allKeys[i];
         var foundTime = parseInt(object.time);
-        engine.log(object.nick + ' ' + object.time);
         if (iterationCount !== 0) break;
       }
 
@@ -219,12 +280,12 @@ iterationCount = 1;
 
 // GROUP ADDER/REMOVER
 if (found) {
-  for(var count = -1; count < config.hoursArray.length; count++) {
+  for(var count = -1; count < config.hours_array.length; count++) {
     
-    if (config.hoursArray.length < 1 || config.hoursArray.length == count+1) { // if there is only one level || or if the level is the last one.
+    if (config.hours_array.length < 1 || config.hours_array.length == count+1) { // if there is only one level || or if the level is the last one.
       var currentStep;
       if (count === -1) currentStep = 0;
-      if (count > -1) currentStep = config.hoursArray[count].hours;
+      if (count > -1) currentStep = config.hours_array[count].hours;
       if (foundTime/3600 > currentStep) {
               if(isNotInServerGroup(clientServerGroups, fg+count+1)) {
                         user.addToServerGroup(fg+count+1);
@@ -240,8 +301,8 @@ if (found) {
     } else {
       var currentStep;
       if (count === -1) currentStep = 0;
-      if (count > -1) currentStep = config.hoursArray[count].hours;
-          if (foundTime/3600 > currentStep && foundTime/3600 < config.hoursArray[count+1].hours) {
+      if (count > -1) currentStep = config.hours_array[count].hours;
+          if (foundTime/3600 > currentStep && foundTime/3600 < config.hours_array[count+1].hours) {
               if(isNotInServerGroup(clientServerGroups, fg+count+1)) {
                         user.addToServerGroup(fg+count+1);
                         user.poke('Congratulations! You have advanced to '+ (count+2) +' LEVEL, spending more than '+ currentStep +' hours!');
@@ -273,32 +334,31 @@ dataArray.length = 0;
 } 
 };
 
+if (!channel) channel = backend.getChannelByID(config.ranking_channel);
+if (!channel) return engine.log("Channel is not defined!");
 // TOP RECORDS SHOWN ON GIVEN CHANNEL
-var sortedArray = sortable.sort(function(a, b){
+var sorted_array = sortable.sort(function(a, b){
     return b[1]-a[1];
 })
+// Filter out ignored sgid's.
+sorted_array = sorted_array.filter(function(row) {
+  return !botList.includes(row[2]);
+});
 
+if (sorted_array.length > limit) sorted_array.length = limit;
 var s = "";
-if (found) {
- for(var counter = 0; counter < limit; counter++) {
-    if (!contains(botList, sortedArray[counter][2])) {
-      var xyz = '[*]' + '[SIZE=16][COLOR='+config.recordaNameColor+'][B]' + sortedArray[counter][0] + '[/B][/COLOR][/SIZE]' + '[SIZE=14][B][COLOR='+config.recordbTimeColor+'] ' + Math.floor(sortedArray[counter][1]/3600/24) + ' [/COLOR][COLOR='+config.recordcFormatColor+']Days[/COLOR][COLOR='+config.recordbTimeColor+'] ' + Math.floor((sortedArray[counter][1]%86400)/3600) + ' [/COLOR][COLOR='+config.recordcFormatColor+']Hours[/COLOR][COLOR='+config.recordbTimeColor+'] ' + Math.floor((sortedArray[counter][1]%3600)/60) + ' [/COLOR][COLOR='+config.recordcFormatColor+']Minutes[/COLOR][/B][/SIZE]'; 
-      s += xyz;    
-    } else {
-    limit++;
-    }
-}
-    var clean_s = s;
+if (!found) return;
+sorted_array.forEach(function(row) {
+  var xyz = '[*]' + '[SIZE=16][COLOR='+list_nick_color+'][B]' + row[0] + '[/B][/COLOR][/SIZE]' + '[SIZE=14][B][COLOR='+list_time_color+'] ' + Math.floor(row[1]/3600/24) + ' [/COLOR][COLOR='+list_format_color+']Days[/COLOR][COLOR='+list_time_color+'] ' + Math.floor((row[1]%86400)/3600) + ' [/COLOR][COLOR='+list_format_color+']Hours[/COLOR][COLOR='+list_time_color+'] ' + Math.floor((row[1]%3600)/60) + ' [/COLOR][COLOR='+list_format_color+']Minutes[/COLOR][/B][/SIZE]'; 
+  s += xyz;
+});
 
+var clean_s = s;
 var zxc = '[list=1]' + clean_s + '[/list]';
-channelUpdate(config.drankingChannel, {description : zxc});
-
-}
+channel.update({description : zxc});
 
 }
 setInterval(channelTOP, delayTime*1000);
-
-
 
   function isNotInServerGroup(user_servergroups, servergroup_id) {
 
